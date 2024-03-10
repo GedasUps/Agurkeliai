9a10dc5dea8007b409a419d5cd1ce2e8323c8b24
GedasUps <83981291+GedasUps@users.noreply.github.com>
2024-03-10 14:01:14 +0200

    upadated user profile window

--- projektas/pirmas/app/src/main/java/com/example/myapplication/ProfileFragment.java
+++ projektas/pirmas/app/src/main/java/com/example/myapplication/ProfileFragment.java
@@ -7,13 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment  {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -52,6 +55,7 @@ public class ProfileFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -60,5 +64,15 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    public  void ChangeContact(View view)
+    {
+        EditText name = getView().findViewById(R.id.edtxtName);
+        EditText email = getView().findViewById(R.id.edtxtName);
+        EditText number = getView().findViewById(R.id.edtxtName);
+        ListView hist = getView().findViewById(R.id.lstHistory);
+        int count  = hist.getFooterViewsCount();
+
     }
 }
