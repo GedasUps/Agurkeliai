b14b94670863647d2ffda4c7e2eab62fac8ed2f4
Nesugalvuojo <modestaslitvinas@gmail.com>
2024-03-28 10:34:48 +0200

    Fixed code format

--- projektas/pirmas/app/src/main/java/com/example/myapplication/MapFragment.java
+++ projektas/pirmas/app/src/main/java/com/example/myapplication/MapFragment.java
@@ -59,8 +59,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     @Override
